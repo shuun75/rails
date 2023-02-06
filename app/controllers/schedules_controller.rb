171df4e280a12ea0b7a1651461dc48f7cd5885cb
宇須俊也 <>
2023-02-07 04:59:39 +0900

    詳細ページ作成

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -2,4 +2,8 @@ class SchedulesController < ApplicationController
   def index
     @schedules = Schedule.all
   end
+
+  def show
+    @schedule = Schedule.find(params[:id])
+  end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,6 @@
 class Schedule < ApplicationRecord
+
+  def is_all_day?
+    "○" if is_all_day == true
+  end
 end
