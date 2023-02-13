3496814681f7eb8c213233f4b23cbc0d3e4ea826
宇須俊也 <>
2023-02-14 02:37:49 +0900

    新規追加機能実装

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -6,4 +6,22 @@ class SchedulesController < ApplicationController
   def show
     @schedule = Schedule.find(params[:id])
   end
+
+  def new
+    @schedule = Schedule.new
+  end
+
+  def create
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      redirect_to root_path, notice: 'Task is create'
+    else
+      render :new
+    end
+  end
+
+  private
+  def schedule_params
+    params.require(:schedule).permit(:title, :start_date, :end_date, :is_all_day, :content)
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module Myapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
   end
 end
